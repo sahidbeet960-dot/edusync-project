aed5acfcc3936b382766d904586d7faa872cbca6
Toufik Mamud <someonecricky@gmail.com>
2026-03-07 15:14:22 +0530

    create the shared forum for all three role

--- Edu-Sync/src/App.jsx
+++ Edu-Sync/src/App.jsx
@@ -12,6 +12,7 @@ import Registration from "./AUTH/Registration";
 // shared component for all three roles
 import SharedProfile from "./Component/SharedProfile";
 import SharedResources from "./Component/SharedResources";
+import SharedForum from "./Component/SharedForum";
 // professor component
 import ProfessorLayout from "./Professor/ProfessorLayout";
 import ProfessorHome from "./Professor/ProfessorHome";
@@ -34,9 +35,14 @@ function App() {
             <Route index element={<ProfessorHome />} />
             <Route path="messages" element={<MessagesPage />} />
             <Route path="notifications" element={<NotificationsPage />} />
-            <Route path="profile" element={<SharedProfile/>}/>
-            <Route path="resources" element={<SharedResources/>}/>
+            <Route path="profile" element={<SharedProfile />} />
+            <Route path="resources" element={<SharedResources />} />
+            <Route path="discussions" element={<SharedForum />} />
           </Route>
+          {/* CR Dashboard */}
+          {/* <Route path="/dashboard/cr" element={<CRLayout />}>
+
+          </Route> */}
         </Routes>
       </div>
     </Router>
